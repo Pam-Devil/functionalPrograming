d5d5582b5b5f1bfbb376632292d9d8de04fc254b
Pam-Devil <sudopamdevil@gmail.com>
2023-05-25 19:01:54 -0300

    finalmente ta começando a avançar essa desgraça

--- Monads/typescript/monads.cpp
+++ Monads/typescript/monads.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
-#include <utility>
-#include <functional>
-#include <string>
-using namespace std;
+#include<string>
 
-template <typename T>
-struct TypeWithLog{
-    pair<T,string> unwrap;
+template <class T>
+concept explicit_bool = std::same_as<T,bool>;
+
+template <explicit_bool T>
+std::pair<bool,std::string> negate(T x) {
+    
+    return std::make_pair(!x,"Not!");
+};
+template<explicit_bool T>
+std::pair<bool,std::string> keep (T x){
+  return std::make_pair(x,"Keep!");  
+};
+template<typename T>
+std::pair<T,std::string> wrap (T x){
+  return std::make_pair(x,"Wrap type!");  
 };
 
-TypeWithLog<bool> negar(bool x){
-    TypeWithLog<bool> type_wrap;
-    type_wrap.unwrap = make_pair(!x,"Not!");
-    return type_wrap;
-};
-TypeWithLog<bool> manter(bool x){
-    TypeWithLog<bool> type_wrap;
-    type_wrap.unwrap = make_pair(x,"Keep");
-    return type_wrap;
-};
 template<typename A, typename B>
-function<TypeWithLog<bool>(A a)> 
-    compose(function<TypeWithLog<B>(A a)>foo,function<TypeWithLog<bool>(B b)>bar){
-        return [foo,bar](A x){
-            auto p1 = foo(x).unwrap;
-            auto p2 = bar(p1.first).unwrap;
-            return make_pair(p2.first,p1.second+p2.second);
-        };
-    };
+std::pair<A,std::string> compose(A a, B b, std::pair<A,std::string> (*func)(A)){
+    return std::make_pair(func(a).first,"Composed!");
+}
+
 int main(){
-    compose(negar,manter);
+
+    auto newcomposed =  wrap(15);
+    std::cout << "The first element of newcompose is: " << newcomposed.first <<'\n';
+    std::cout <<"The second element of newcompose is: " << newcomposed.second <<'\n';
+    
+    auto composed =  compose(false, true, &negate);
+    std::cout <<std::boolalpha << "The first element of compose is: " << composed.first <<'\n';
+    std::cout <<"The second element of compose is: " << composed.second <<'\n';
+    
+    
     return 0;
-};
+}
