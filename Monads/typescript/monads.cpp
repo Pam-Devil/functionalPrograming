8eb4b1f71d57e84959479be96b2e5f64c20a0f86
PamDevil <sudopamdevil@gmail.com>
2023-05-25 10:56:10 -0300

    fuck

--- Monads/typescript/monads.cpp
+++ Monads/typescript/monads.cpp
@@ -4,32 +4,31 @@
 #include <string>
 using namespace std;
 
-
 template <typename T>
 struct TypeWithLog{
     pair<T,string> unwrap;
 };
 
-pair<bool, string> negar(bool x){
+TypeWithLog<bool> negar(bool x){
     TypeWithLog<bool> type_wrap;
     type_wrap.unwrap = make_pair(!x,"Not!");
-    return type_wrap.unwrap;
+    return type_wrap;
 };
-pair<bool, string> manter(bool x){
+TypeWithLog<bool> manter(bool x){
     TypeWithLog<bool> type_wrap;
     type_wrap.unwrap = make_pair(x,"Keep");
-    return type_wrap.unwrap;
+    return type_wrap;
 };
 template<typename A, typename B>
 function<TypeWithLog<bool>(A a)> 
-    compose(function<TypeWithLog<B>>(A a)>foo,function<TypeWithLog<bool>>(B b)>bar){
+    compose(function<TypeWithLog<B>(A a)>foo,function<TypeWithLog<bool>(B b)>bar){
         return [foo,bar](A x){
             auto p1 = foo(x).unwrap;
             auto p2 = bar(p1.first).unwrap;
             return make_pair(p2.first,p1.second+p2.second);
         };
     };
-
 int main(){
-    printf("%d",negar(false));
+    compose(negar(false),manter(true));
+    return 0;
 };
